6f3cc2f2ab56c08d81ce52d8e8763e1ef5695162
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-14 19:21:59 +0800

    [Reduce] [Hannah] add: add Reduce.java

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Reduce {
@@ -13,14 +14,21 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+
+        Integer result = arrayList.stream().reduce(0, (sum,current) -> {
+            sum += current;
+            return sum;
+        });
+        return new BigDecimal(result).divide(new BigDecimal(arrayList.size())).doubleValue();
+
     }
 
     public int getMaxValue() {
-        throw new NotImplementedException();
+
+        return arrayList.stream().reduce(0, Integer::max);
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(integer -> integer % 2 == 1).reduce((first, second) -> second).get();
     }
 }
