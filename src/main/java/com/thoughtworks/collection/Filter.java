60f7c998ac5b6ae098d17053512776aa9f20f507
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-14 19:20:26 +0800

    [Filter] [Hannah] add: add Filter.java

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -3,6 +3,9 @@ package com.thoughtworks.collection;
 import org.apache.commons.lang3.NotImplementedException;
 import java.util.List;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Filter {
 
     List<Integer>  array;
@@ -12,14 +15,15 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+
+        return array.stream().filter(integer -> integer % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream().filter(secondList :: contains).collect(Collectors.toList());
     }
 }
